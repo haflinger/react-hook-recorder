b2ed634a0f750c6db77117e47eed69c72f64ec3d
Jean-Charles Bailliez <jean-charles.bailliez@lemonlearning.fr>
2022-10-25 14:01:20 +0200

    🚨 Add storybook eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,10 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "prettier", "plugin:storybook/recommended"],
   settings: {
     react: {
-      version: "detect",
-    },
-  },
+      version: "detect"
+    }
+  }
 };
